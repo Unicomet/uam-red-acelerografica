301adccb5ef6e51007a93d0acde644cf06fbca9b
Guillermo Montano <martinezguillermo559@gmail.com>
2025-02-08 13:54:45 -0600

    fix(i18n): ensure language switching works on mobile
    Previously, language switching only worked on desktop but was broken on mobile.
    This fix ensures that the functionality behaves correctly across all screen sizes.

--- assets/js/language-switching.js
+++ assets/js/language-switching.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let storedLang = "es";
-    const langSwitcher = document.getElementById("lang-switcher");
-
+function changeLanguage(langSwitcher) {
     if (localStorage.getItem("language")) {
         storedLang = localStorage.getItem("language");
         if (storedLang == "es") {
@@ -14,11 +11,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
         window.i18n.setLanguage(storedLang);
     }
+}
 
-    langSwitcher.addEventListener("change", () => {
-        const newLang = langSwitcher.value;
-        localStorage.setItem("language", newLang);
+// document.addEventListener("DOMContentLoaded", () => {
+//     let storedLang = "es";
+//     const langSwitcher = document.getElementById("lang-switcher");
 
-        window.i18n.setLanguage(newLang);
-    });
-});
+//     if (localStorage.getItem("language")) {
+//         storedLang = localStorage.getItem("language");
+//         if (storedLang == "es") {
+//             langSwitcher.children[0].setAttribute("selected", "");
+//             langSwitcher.children[1].removeAttribute("selected");
+//         } else {
+//             langSwitcher.children[1].setAttribute("selected", "");
+//             langSwitcher.children[0].removeAttribute("selected");
+//         }
+
+//         window.i18n.setLanguage(storedLang);
+//     }
+
+//     langSwitcher.addEventListener("change", () => {
+//         const newLang = langSwitcher.value;
+//         localStorage.setItem("language", newLang);
+
+//         window.i18n.setLanguage(newLang);
+//     });
+// });
